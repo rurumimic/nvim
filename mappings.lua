9f9085ce2e9b2925f3efb7ec286c3f6499e8f964
rurumimic <10915235+rurumimic@users.noreply.github.com>
2023-08-06 15:35:00 +0900

    control key up down

--- mappings.lua
+++ mappings.lua
@@ -3,8 +3,12 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<F5>"] = { ":UndotreeToggle<CR>", "Toggle Undotree", opts = { nowait = true }},
+
+    ["<F5>"] = { ":UndotreeToggle<CR>", "Toggle Undotree", opts = { nowait = true } },
     ["<F8>"] = { ":TagbarToggle<CR>", "Toggle Tagbar", opts = { nowait = true } },
+
+    ["<C-u>"] = { "<C-u>zz", opts = { nowait = true } },
+    ["<C-d>"] = { "<C-d>zz", opts = { nowait = true } },
   },
 }
 
