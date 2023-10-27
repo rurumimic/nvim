5ef9f0793335f34a3dc5c24f9aff3e93763145e5
rurumimic <10915235+rurumimic@users.noreply.github.com>
2023-10-28 01:27:51 +0900

    lldb-vscode

--- modules/lsp.lua
+++ modules/lsp.lua
@@ -26,9 +26,12 @@ return {
         -- "clangd",
         "clang-format",
 
-        "gopls",
         "rust-analyzer",
+        -- "lldb-vscode", -- already installed by LLVM
+
+        "gopls",
         -- "zls",
+
         "pyright","black","isort",
         "debugpy",
 
@@ -54,8 +57,9 @@ return {
         "c",
         "cpp",
 
-        "go",
         "rust",
+
+        "go",
         -- "zig",
 
         "python",
@@ -81,29 +85,82 @@ return {
   },
   {
     "mfussenegger/nvim-dap",
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
     config = function()
-      require("mason").setup()
-      require("mason-nvim-dap").setup({
-        ensure_installed = {
-          "python",
-        },
-        automatic_installation = true,
-        handlers = {
-          function(config)
-            require('mason-nvim-dap').default_setup(config)
+      local dap = require('dap')
+      dap.adapters.lldb = {
+        type = 'executable',
+        command = '/usr/bin/lldb-vscode',
+        name = 'lldb'
+      }
+      dap.configurations.cpp = {
+        {
+          name = 'Launch',
+          type = 'lldb',
+          request = 'launch',
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
           end,
-        },
-      })
+          cwd = '${workspaceFolder}',
+          stopOnEntry = false,
+          args = function()
+            local args_string = vim.fn.input("Input arguments: ")
+            return vim.split(args_string, " ")
+          end,
+          env = function()
+            local variables = {}
+            for k, v in pairs(vim.fn.environ()) do
+              table.insert(variables, string.format("%s=%s", k, v))
+            end
+            return variables
+          end,
+        }
+      }
+      dap.configurations.c = dap.configurations.cpp
+      dap.configurations.rust = {
+          {
+            name = 'Launch',
+            type = 'lldb',
+            request = 'launch',
+            program = function()
+              return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug/', 'file')
+            end,
+            cwd = '${workspaceFolder}',
+            stopOnEntry = false,
+            args = {},
+            initCommands = function()
+              -- Find out where to look for the pretty printer Python module
+              local rustc_sysroot = vim.fn.trim(vim.fn.system('rustc --print sysroot'))
+
+              local script_import = 'command script import "' .. rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py"'
+              local commands_file = rustc_sysroot .. '/lib/rustlib/etc/lldb_commands'
+
+              local commands = {}
+              local file = io.open(commands_file, 'r')
+              if file then
+                for line in file:lines() do
+                  table.insert(commands, line)
+                end
+                file:close()
+              end
+              table.insert(commands, 1, script_import)
+
+              return commands
+            end,
+            env = function()
+              local variables = {}
+              for k, v in pairs(vim.fn.environ()) do
+                table.insert(variables, string.format("%s=%s", k, v))
+              end
+              return variables
+            end,
+          }
+        }
     end,
   },
   {
     "mfussenegger/nvim-dap-python",
     dependencies = {
       "mfussenegger/nvim-dap",
-      "jay-babu/mason-nvim-dap.nvim",
     },
     ft = { "python" },
     config = function(plugin, opts)
