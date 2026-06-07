a9fe6bbd4f81f1e352e389a2fdd2604613b46e34
rurumimic <10915235+rurumimic@users.noreply.github.com>
2026-06-08 08:50:05 +0900

    nvim tree group emtpy

---
+++ lua/plugins/tree.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {
+        renderer = {
+          group_empty = true,
+        },
+      }
+    end,
+  },
+}
